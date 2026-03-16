3587eaca5e95514652830a80756721c83762e9dd
yujiunn20 <yuhjiunn20@gmail.com>
2026-03-16 13:24:38 +0800

    deploy linux server version

--- server/server.js
+++ server/server.js
@@ -448,6 +448,6 @@ app.post("/api/upload-lesson", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server running on http://localhost:3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("server running on http://0.0.0.0:3000");
 });
